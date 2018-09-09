8cb398164dec0e6898a41ed8d35dc3d8b1322c1d
khui88 <khui@outlook.com>
2018-09-09 00:03:05 -0400

    change master dispatcher

--- src/main/java/com/revature/servlets/MasterDispatcher.java
+++ src/main/java/com/revature/servlets/MasterDispatcher.java
@@ -14,25 +14,25 @@ public class MasterDispatcher {
 	public static Object process(HttpServletRequest request, HttpServletResponse response) {
 				
 		switch (request.getRequestURI()) {
-		case "/ers/login.ng":
+		case "/project1/login.ng":
 			return ErsUserService.login(request, response);
 //		case "/ers/logout.ng":
 //			return ErsUserService.logout(request, response);
 //		case "/ers/getReimbursementsByUser.ng":
 //			return  ErsReimbursementService.getReimbursementsByUsername(request,response);
-		case "/ers/getReimbursementsByUserId.ng":
+		case "/project1/getReimbursementsByUserId.ng":
 			return  ErsReimbursementService.getReimbursementsByUserId(request,response);
-		case "/ers/addReimbursement.ng":
+		case "/project1/addReimbursement.ng":
 			return ErsReimbursementService.addReimbursement(request, response);
-		case "/ers/getAllReimbursements.ng":
+		case "/project1/getAllReimbursements.ng":
 			return ErsReimbursementService.getAllReimbursements(request, response);
-		case "/ers/approveReimbursement.ng":
+		case "/project1/approveReimbursement.ng":
 			ErsReimbursementService.approveReimbursement(request, response);
 			return null;
-		case "/ers/denyReimbursement.ng":
+		case "/project1/denyReimbursement.ng":
 			ErsReimbursementService.denyReimbursement(request, response);
 			return null;
-		case "/ers/getAllReimbType.ng":
+		case "/project1/getAllReimbType.ng":
 			return  ErsReimbTypeService.getAll();
 		default:
 			return "bad";
