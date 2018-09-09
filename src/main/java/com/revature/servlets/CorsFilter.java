2657d2bb6e793a6a163e0f2c9729ec79068192dc
khui88 <khui@outlook.com>
2018-09-09 00:30:17 -0400

    changed origin control

--- src/main/java/com/revature/servlets/CorsFilter.java
+++ src/main/java/com/revature/servlets/CorsFilter.java
@@ -20,7 +20,7 @@ public class CorsFilter implements Filter {
 		
 		System.out.println("Incoming " + httpRequest.getMethod() + " request at " + httpRequest.getRequestURI());
 		
-		httpResponse.addHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+		httpResponse.addHeader("Access-Control-Allow-Origin", "http://khui88.github.io/");
 		httpResponse.addHeader("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS");
 		httpResponse.addHeader("Access-Control-Allow-Headers", "Content-Type");
 		
